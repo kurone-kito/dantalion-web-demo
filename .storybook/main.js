b9072e174b691b35a1505b47b10d8e4a90bd2a56
kurone-kito <krone@kit.black>
2022-02-03 21:48:23 +0900

    chore(D): implemented the Storybook environment

---
+++ .storybook/main.js
@@ -0,0 +1,56 @@
+/**
+ * @typedef {import("@babel/core").TransformOptions} TransformOptions
+ * @typedef {import("@storybook/core-common").Options} Options
+ * @typedef {import("@storybook/core-common").StorybookConfig} StorybookConfig
+ */
+
+/**
+ * @template T
+ * @typedef {import("type-fest").Promisable<T>} Promisable
+ */
+
+/**
+ * the configuration for the storybook
+ *
+ * @type {StorybookConfig}
+ */
+module.exports = {
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    '@storybook/addon-interactions',
+    {
+      name: 'storybook-addon-turbo-build',
+      options: {
+        esbuildMinifyOptions: { target: 'es2021' },
+        optimizationLevel: 2,
+      },
+    },
+  ],
+  /** @type {(options: TransformOptions) => Promisable<TransformOptions>} */
+  babel: ({ plugins = [], ...options }) => ({
+    ...options,
+    plugins: [
+      ...plugins,
+      ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+    ],
+  }),
+  core: { builder: 'webpack5' },
+  features: {
+    argTypeTargetsV7: true,
+    breakingChangesV7: true,
+    buildStoriesJson: true,
+    emotionAlias: true,
+    modernInlineRender: true,
+    postcss: true,
+    warnOnLegacyHierarchySeparator: true,
+  },
+  framework: '@storybook/react',
+  /** @type {(head: string, options: Options) => Promisable<string | undefined>} */
+  managerHead: (head, { configType }) =>
+    configType === 'PRODUCTION'
+      ? `<base href="${process.env.CI ? '/dantalion' : ''}/storybook/">${head}`
+      : undefined,
+  stories: [{ directory: '../src' }],
+  typescript: { check: true, reactDocgen: 'react-docgen-typescript' },
+};
