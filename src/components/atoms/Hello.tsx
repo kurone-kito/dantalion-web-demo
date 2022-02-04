1973537f69cd7e1f0d0738c18da745562a49d1ae
kurone-kito <krone@kit.black>
2022-02-04 09:41:09 +0900

    feat: added a dummy component

---
+++ src/components/atoms/Hello.tsx
@@ -0,0 +1,9 @@
+import type { VFC } from 'react';
+
+/**
+ * Hello, world!
+ *
+ * @returns The hello, world! component.
+ */
+export const Hello: VFC = () => <p>Hello, world!</p>;
+Hello.displayName = 'Hello';
