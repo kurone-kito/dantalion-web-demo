3bf1c131d071b36cf46bad1f89802d67b18d40ee
kurone-kito <krone@kit.black>
2022-02-04 14:27:03 +0900

    feat(D): added the twin.macro environment

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,16 @@
-import type { Parameters } from '@storybook/addons';
-import 'tailwindcss/tailwind.css';
+import type { DecoratorFunction, Parameters } from '@storybook/addons';
+import * as React from 'react';
+import { GlobalStyles } from 'twin.macro';
+
+export const decorators: DecoratorFunction[] = [
+  (Story) => (
+    <>
+      <GlobalStyles />
+      {/* @ts-ignore */}
+      <Story />
+    </>
+  ),
+];
 
 export const parameters: Parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
