8e31f434cd3dbecdf9531f6379affdd2f57d8776
kurone-kito <krone@kit.black>
2025-05-09 14:12:58 +0900

    feat: implemented the empty daisy-ui environment

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,6 @@ import './app.css';
  * The main application component.
  * @returns The component.
  */
-const App: Component = () => <h1>Hello, world!</h1>;
+const App: Component = () => <h1 class="text-primary">Hello, world!</h1>;
 
 export default App;
